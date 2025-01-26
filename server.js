4ca5322a93ae45a817edcbae7b3021b5a127a6fd
DodkoSVK <44491257+DodkoSVK@users.noreply.github.com>
2025-01-26 15:46:55 +0100

    First routes

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require('express');
-require('dotenv').config()
+require('dotenv').config();
+const Joi = require("joi");
+
+const barSchema = Joi.object({
+    barName: Joi.string(),
+    barCity: Joi.string(),
+    barStreet: Joi.string(),
+    barPostal: Joi.string(),
+    createdBy: Joi.number().min(1),
+    createDate: Joi.date()
+});
 
 const app = express();
 const PORT = process.env.PORT;
@@ -14,7 +24,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use(logger);
 
 app.get("/", (req, res) => {
+    res.status(202).send({message: "Get / is working"});    
 });
+
+app.post("/cafe/bar", (req, res) => {
+    const { barName, barCity, barStreet, barPostal, createdBy, createDate } = req.body;
+    console.log(`I get: ${barName}, ${barCity}, ${barStreet}, ${barPostal}, ${createdBy}, ${createDate}`);
+    const {error} = barSchema.validate(req.body)
+    if (error) {
+        res.status(400).send(error.message);
+    }    
+    res.status(202).send({message: `I get: Bar:${barName}, City: ${barCity}, Street: ${barStreet}, Postal: ${barPostal}, Created By: ${createdBy}, Create Date: ${createDate}`});
+});
+
+app.post("/cafe/employee", (res, req) => {
+    
+})
 
 app.listen(PORT, () => {
     console.log(`🟢 Server is on. Listening on port ${PORT}`);
